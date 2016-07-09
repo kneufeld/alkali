ab7fc480ba6d6bb56f9664cf4b98979f6d140f23
Kurt Neufeld <kneufeld@burgundywall.com>
2016-07-09 17:21:56 -0600

    adding ModelManager and tests

---
+++ redb/manager.py
@@ -0,0 +1,60 @@
+from zope.interface import Interface, Attribute, implements
+import copy
+import os
+
+from .model import Model
+
+class IManager( Interface ):
+
+    pass
+    # modified   = Attribute("are any fields dirty")
+    # fields     = Attribute("the dict of fields")
+
+class Manager(object):
+    """
+    main class for any database. this holds the actual data of the database.
+    """
+    implements(IManager)
+
+    def __init__( self, model_class, *args, **kw ):
+        self._model_class = model_class
+        self._instances = {}
+
+    def __str__(self):
+        try:
+            basename = ' ' + os.path.basename( self.filename )
+        except AttributeError:
+            basename = ''
+        return "<{name}{fname}>".format(name=self.name, fname=basename)
+
+    def __len__(self):
+        return len(self._instances)
+
+    @property
+    def name(self):
+        return "{}Manager".format(self._model_class.__name__)
+
+    def get(self, pk):
+        return self._instances[pk]
+
+    def save(self, instance):
+        assert instance.pk
+        self._instances[ instance.pk ] = instance
+
+    def delete(self, instance):
+        try:
+            del self._instances[ instance.pk ]
+        except KeyError:
+            pass
+
+    def store(self, storage):
+        "save all our instances to storage"
+        storage.write( [m.dict for pk,m in self._instances.items()] )
+
+    def load(self, storage):
+        "load all our instances from storage"
+        data = storage.read() # data is a list of dicts
+
+        for elem in data:
+            m = self._model_class( **elem )
+            self.save(m)
---
+++ redb/tests/test_manager.py
@@ -0,0 +1,122 @@
+import os
+import unittest
+import tempfile
+from zope.interface.verify import verifyObject, verifyClass
+import datetime as dt
+
+from redb.model import Model
+from redb.manager import IManager, Manager
+from redb.storage import JSONStorage
+from redb import fields
+from redb import tznow
+
+class EmptyModel(Model):
+    pass
+
+class MyModel( Model ):
+    int_type = fields.IntField(primary_key=True)
+    str_type = fields.StringField()
+    dt_type  = fields.DateField()
+
+class TestManager( unittest.TestCase ):
+
+    def test_1(self):
+        "verify class/instance implementation"
+        self.assertTrue( verifyClass(IManager, Manager) )
+
+        man = Manager(MyModel)
+        self.assertTrue( verifyObject(IManager, man) )
+        self.assertEqual( "MyModelManager", man.name )
+
+    def test_2(self):
+        "test saving"
+
+        man = Manager(MyModel)
+
+        # no primary key in m1
+        m1 = MyModel()
+        self.assertRaises( AssertionError, man.save, m1 )
+
+        # successful save
+        m1.int_type = 1
+        man.save(m1)
+        self.assertEqual( 1, len(man) )
+
+        # same primary key, same len
+        man.save(m1)
+        self.assertEqual( 1, len(man) )
+
+        # different primary key, new len
+        m1.int_type = 2
+        man.save(m1)
+        self.assertEqual( 2, len(man) )
+
+    def test_3(self):
+        "test deleting"
+
+        man = Manager(MyModel)
+
+        m1 = MyModel(int_type=1)
+        man.save(m1)
+        self.assertEqual( 1, len(man) )
+
+        man.delete(m1)
+        self.assertEqual( 0, len(man) )
+
+        # make sure deleting a non-existant instance is a no-op
+        man.delete(m1)
+        self.assertEqual( 0, len(man) )
+
+    def test_4(self):
+        "test getting"
+
+        man = Manager(MyModel)
+
+        m1 = MyModel(int_type=1)
+        man.save(m1)
+
+        m2 = man.get(m1.pk)
+        self.assertDictEqual( m1.dict, m2.dict )
+
+        # does not exist
+        self.assertRaises( KeyError, man.get, 2 )
+
+    def test_5(self):
+        "test saving actual model objects"
+
+        tfile = tempfile.NamedTemporaryFile()
+
+        now = tznow()
+        m = MyModel(int_type=3, str_type='string', dt_type=now )
+        d={u'int_type':3, u'str_type':u'string', u'dt_type':now.isoformat()}
+
+        man = Manager(MyModel)
+        man.save(m)
+
+        storage = JSONStorage(tfile.name)
+        man.store( storage )
+
+        from_disk = storage.read()
+        self.assertDictEqual( d, from_disk[0] )
+
+    def test_6(self):
+        "test loading actual model objects"
+
+        tfile = tempfile.NamedTemporaryFile()
+
+        now = tznow()
+        m1 = MyModel(int_type=3, str_type='string', dt_type=now )
+
+        man = Manager(MyModel)
+        man.save(m1)
+
+        storage = JSONStorage(tfile.name)
+        man.store( storage )
+
+        man = Manager(MyModel)
+        self.assertRaises( KeyError, man.get, m1.pk )
+
+        man.load( storage )
+        m2 = man.get(m1.pk)
+
+        self.assertDictEqual( m1.dict, m2.dict )
