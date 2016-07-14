d4c559545f7425bf581c5607719ce331fde85ce6
Kurt Neufeld <kneufeld@burgundywall.com>
2016-07-13 20:57:47 -0600

    big refactor of metamodel but no real functionality change

--- redb/metamodel.py
+++ redb/metamodel.py
@@ -30,65 +30,47 @@ class MetaModel(type):
         parents = [b for b in bases if isinstance(b, MetaModel)]
         if not parents:
             new_class = super_new(meta_class, name, bases, attrs)
-            #meta_class._add_fields( new_class, {}, {} )
             return new_class
 
         # new_class is an instance of 'name' (aka Model) whose type is MetaModel
-        new_class = super_new(meta_class, name, bases, {})
         # print "new_class", type(new_class), new_class
         # new_class <class 'redb.metamodel.MetaModel'> <class 'redb.metamodel.MyModel'>
-
-        new_class._prepare( attrs )
-        #new_class._meta.apps.register_model(new_class._meta.app_label, new_class)
-
-        return new_class
-
-    # creates a new instance of derived model
-    def __call__(self, *args, **kw):
-
-        kw_fields={}
-        for name,_ in self.Meta.fields.items():
-            kw_fields[name] = kw.pop(name, None)
-
-        obj = type.__call__(self, *args, **kw)
-
-        for name, value in kw_fields.items():
-            value = self.Meta.fields[name].cast(value)
-            setattr(obj, name, value)
-
-        return obj
-
-    def _prepare( new_class, attrs ):
-        class Object(object): pass
-
-        meta = attrs.pop( 'Meta', Object() )
-        setattr( new_class, 'Meta', meta )
-
-        setattr( new_class, 'name', new_class.__name__ )
-
-        if not hasattr(new_class.Meta, 'filename'):
-            new_class.Meta.filename = ''
-
-        if not hasattr(new_class.Meta, 'storage'):
-            new_class.Meta.storage = ''
-
-        if not hasattr(new_class.Meta, 'ordering'):
-            new_class.Meta.ordering = []
-
-        new_class._add_fields( new_class.Meta, attrs )
-        new_class._add_manager( new_class.Meta, attrs )
+        new_class = super_new(meta_class, name, bases, {})
+        new_class._add_meta( attrs )
+        new_class._add_manager()
 
         # put the rest of the attributes (methods and properties)
         # defined in the Model derived class into the "new" Model
-        for k,v in attrs.items():
-            setattr(new_class, k, v)
+        for name, attr in attrs.items():
+            setattr(new_class, name, attr)
+
+        setattr( new_class, 'name', new_class.__name__ )
+
+        return new_class
+
+    def _add_manager( new_class ):
+        from .manager import Manager
+        setattr( new_class, 'objects', Manager(new_class) )
 
     @property
     def pk_fields(new_class):
         return [name for name,field in new_class.Meta.fields.items() if field.primary_key]
 
-    def _add_fields( new_class, meta, attrs):
-        "put fields in meta class"
+    def _add_meta( new_class, attrs ):
+        class Object(object): pass
+
+        meta = attrs.pop( 'Meta', Object() )
+        setattr( new_class, 'Meta', meta )
+
+        if not hasattr(meta, 'filename'):
+            meta.filename = ''
+
+        if not hasattr(meta, 'storage'):
+            meta.storage = ''
+
+        if not hasattr(meta, 'ordering'):
+            meta.ordering = []
+
         ordering = meta.ordering or [k for k,v in attrs.items() if isinstance(v,Field)]
 
         meta.fields = OrderedDict()
@@ -100,6 +82,17 @@ class MetaModel(type):
         # meta.pk_fields is a property
         meta.pk_fields = new_class.pk_fields
 
-    def _add_manager( new_class, meta, attrs ):
-        from .manager import Manager
-        setattr( new_class, 'objects', Manager(new_class) )
+    # creates a new instance of derived model
+    def __call__(self, *args, **kw):
+        kw_fields={}
+        for name,_ in self.Meta.fields.items():
+            kw_fields[name] = kw.pop(name, None)
+
+        # obj is a instance of Model (or a derived class)
+        obj = type.__call__(self, *args, **kw) # this calls obj.__init__
+
+        for name, value in kw_fields.items():
+            value = self.Meta.fields[name].cast(value)
+            setattr(obj, name, value)
+
+        return obj
