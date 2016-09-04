e2122e42b44e0d5d36bdb6cb0c4289efeadfe355
Kurt Neufeld <kneufeld@burgundywall.com>
2016-09-04 13:02:18 -0600

    dramatically improved database docstrings

--- alkali/database.py
+++ alkali/database.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from zope.interface import Interface, Attribute, implements
 import datetime as dt
 from collections import OrderedDict
@@ -14,16 +16,33 @@ logger = logging.getLogger(__name__)
 
 class IDatabase( Interface ):
 
-    models   = Attribute("the dict of models")
+    models   = Attribute("the ``dict`` of models")
 
 class Database(object):
     """
-    This is the glue object that owns and coordinates all the different
-    classes and objects.
+    This is the parent object that owns and coordinates all the different
+    classes and objects defined in this module.
+
+    :ivar _storage_type:
+        default storage type for all models, defaults to :class:`alkali.storage.JSONStorage`
+
+    :ivar _root_dir:
+        directory where all models are stored, defaults to current working directory
+
+    :ivar _save_on_exit:
+        automatically save all models before Database object is destroyed. call
+        :func:`Database.store` explicitly if ``_save_on_exit`` is false.
     """
     implements(IDatabase)
 
-    def __init__( self, models=[], *args, **kw ):
+    def __init__( self, models=[], **kw ):
+        """
+        :param models: a list of :class:`alkali.model.Model` classes
+        :param kw:
+            * storage: default storage class for all models
+            * root_dir: default save path directory
+            * save_on_exit: save all models to disk on exit
+        """
 
         logger.debug( "Database: creating database" )
 
@@ -42,9 +61,17 @@ class Database(object):
 
     @property
     def models(self):
+        """
+        **property**: return ``list`` of models in the database
+        """
         return self._models.values()
 
     def get_model(self, model_name):
+        """
+        :param model_name: the name of the model, **note**: all model names are
+            converted to lowercase
+        :rtype: :class:`alkali.model.Model`
+        """
         try:
             return self._models[model_name.lower()]
         except KeyError:
@@ -54,8 +81,16 @@ class Database(object):
 
     def get_filename(self, model, storage=None):
         """
-        allow models to specify their own filename or use
-        storage extension default
+        get the filename for the specified model. allow models to
+        specify their own filename or generate one based on storage
+        class. prepend Database._root_dir.
+
+        eg. <_root_dir>/<model name>.<storage.extension>
+
+        :param model: the model name or model class
+        :param storage: the storage class, uses the database default if None
+        :return: returns a filename path
+        :rtype: str
         """
         if isinstance(model, types.StringTypes):
             model = self.get_model(model)
@@ -70,9 +105,15 @@ class Database(object):
 
     def get_storage(self, model, default=None, filename=None):
         """
-        allow models to specify their own storage or use
-        database default
+        get the storage class for the specified model. allow models to
+        specify their own storage or use database default.
+
+        :param model: the model name or model class
+        :param default: override database default storage class
+        :param filename: override database default filename
         """
+        # FIXME this should return a class not instance
+
         if isinstance(model, types.StringTypes):
             model = self.get_model(model)
 
@@ -86,8 +127,15 @@ class Database(object):
 
     def store(self, filename=None, storage=None, force=False):
         """
-        save the data for each model
+        write all model data to disk
+
+        :param filename: override model filename
+        :param storage: override model storage class
+        :param force: force store even if :class:`alkali.manager.Manager`
+            thinks data is clean
         """
+        # FIXME can't specify filename if more than one model in database
+
         logger.debug( "Database: storing models" )
 
         # make sure storage is an instantiated class
@@ -101,8 +149,12 @@ class Database(object):
 
     def load(self, filename=None, storage=None):
         """
-        load the data for each model
+        load all model data from disk
+
+        :param filename: override model filename
+        :param storage: override model storage class
         """
+        # FIXME can't specify filename if more than one model in database
         logger.debug( "Database: loading models" )
 
         # make sure storage is an instantiated class
