6a639144e1851fc0565c5616636a6732b139d589
Kurt Neufeld <kneufeld@burgundywall.com>
2016-09-04 12:59:46 -0600

    changing how docs are generated

--- alkali/__init__.py
+++ alkali/__init__.py
@@ -3,6 +3,7 @@ __version__ = '0.5.0'
 __author__ = 'Kurt Neufeld'
 __author_email__ = 'kneufeld@burgundywall.com'
 __license__ = 'MIT License'
+__url__ = 'https://github.com/kneufeld/alkali'
 __copyright__ = 'Copyright 2016 to ~2086 Kurt Neufeld'
 
 from .database import Database
